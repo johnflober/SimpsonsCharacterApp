ee8b46c58ab3ca60ba3122ea5f90a4ffe03a41ab
Goober <programmingpeanut@gmail.com>
2020-02-26 22:04:40 -0600

    Set up app basics for initial run

---
+++ simpsonscharacter-app-ws/simpsonscharacter-app-ws/src/main/java/com/funapps/app/ws/SimpsonscharacterAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.funapps.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpsonscharacterAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpsonscharacterAppWsApplication.class, args);
+	}
+
+}
---
+++ simpsonscharacter-app-ws/simpsonscharacter-app-ws/src/test/java/com/funapps/app/ws/SimpsonscharacterAppWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funapps.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpsonscharacterAppWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
